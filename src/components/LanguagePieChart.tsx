06c1755d3494559d7980c826f64897a0b2ac69e0
Nathan Forest <hoshie@gmail.com>
2026-03-29 10:06:43 +1000

    Corect Percent through recharts

--- src/components/LanguagePieChart.tsx
+++ src/components/LanguagePieChart.tsx
@@ -27,7 +27,7 @@ export function LanguagePieChart() {
               cx="50%"
               cy="50%"
               labelLine={false}
-              label={({ name, percentage }) => `${name} ${percentage.toFixed(0)}%`}
+              label={({ name, percent }) => `${name} ${((percent ?? 0) * 100).toFixed(0)}%`}
               outerRadius={80}
               fill="#8884d8"
               dataKey="value"
