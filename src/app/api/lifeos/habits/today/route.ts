2c4c61d8fa26e64bcdb2897077bffec93dfa5820
Nathan Forest <hoshie@gmail.com>
2026-04-03 16:17:17 +1000

    fix: Today API to reflexct today

--- src/app/api/lifeos/habits/today/route.ts
+++ src/app/api/lifeos/habits/today/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   const token = request.headers.get('Authorization');
   try {
-    const response = await fetch('http://192.168.50.160:5005/api/habits', {
+    const response = await fetch('http://192.168.50.160:5005/api/habits/today', {
       headers: { 'Authorization': token || '', 'Content-Type': 'application/json' },
       cache: 'no-store',
     });
